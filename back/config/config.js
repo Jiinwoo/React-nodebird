9460177f07ee8f6fc71edd304bc5bcfbd063c1d1
jinwoojung <jwjung5038@gmail.com>
2019-11-18 23:51:20 +0900

    mysql docker 변경

--- back/config/config.js
+++ back/config/config.js
@@ -8,20 +8,22 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": "react-nodebird",
     "host": "127.0.0.1",
+    "port" : 3306,
     "dialect": "mysql"
+    
   },
   "test": {
     "username": "root",
     "password": process.env.DB_PASSWORD,
     "database": "react-nodebird",
-    "host": "127.0.0.1",
+    "host": "mysql",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
     "password": process.env.DB_PASSWORD,
     "database": "react-nodebird",
-    "host": "127.0.0.1",
+    "host": "mysql",
     "dialect": "mysql"
   }
 }
